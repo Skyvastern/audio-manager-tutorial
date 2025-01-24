4d7df3b5fea134736a0db8d0594bf96d3b98b3e9
Arpit Srivastava <skyvastern@gmail.com>
2025-01-24 19:50:26 +0530

    Remove hiding of mouse

--- main/ui/pause_ui/pause_ui.gd
+++ main/ui/pause_ui/pause_ui.gd
@@ -31,9 +31,3 @@ func _on_exit_btn_pressed() -> void:
 
 func _on_pause_update() -> void:
 	visible = get_tree().paused
-	
-	if get_tree().paused:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		AudioManager.play_window_audio()
-	else:
-		Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
